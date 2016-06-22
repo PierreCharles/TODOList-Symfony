f7c893db6dd5ce9785ff56638dd3223bf0817bb9
Julien BRAT <bratjulien@gmail.com>
2016-06-22 12:02:03 +0200

    refonte module GoogleAPI, commentaire

--- src/GoogleApiTaskBundle/Controller/HomeController.php
+++ src/GoogleApiTaskBundle/Controller/HomeController.php
@@ -5,16 +5,26 @@ namespace GoogleApiTaskBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\User\User;
 
 class HomeController extends Controller
 {
+    /**
+     * index Action to have the menu for the google TodoList
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('GoogleApiTaskBundle:Home:index.html.twig');
     }
 
+    /**
+     * Callback Function for Oauth authorization with google API, set the Token for a user
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function callbackAction(Request $request)
     {
         if ($request->query->get('error')) {
@@ -39,41 +49,67 @@ class HomeController extends Controller
         return $this->redirectToRoute('google_task_api_list');
     }
 
-
+    /**
+     * Logout Action, remove the token
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function logoutAction()
     {
-
         $securityStorage = $this->get('security.token_storage');
         $securityStorage->setToken(null);
 
         return $this->redirectToRoute('homepage');
     }
 
+    /**
+     * Method to list all the Task from the Google API Tasks
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function listAction()
     {
         $taskService = $this->get('google_task_api.google.home');
         $taskLists = $taskService->getTaskLists();
 
-        foreach ($taskLists as $taskList) {
-            $tasks = $taskService->getTasksFromList($taskList->getId());
-        }
-
         return $this->render('GoogleApiTaskBundle:Home:index.html.twig', array('taskLists' => $taskLists));
     }
 
+    /**
+     * Methode to delete a taskList to Google API Tasks
+     *
+     * @param $taskList
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function deleteAction($taskList)
     {
         $taskService = $this->get('google_task_api.google.home');
         $taskService->deleteTasksList($taskList);
+
         return $this->redirectToRoute('google_task_api_list');
     }
 
+    /**
+     * Method to update a task List
+     *
+     * @param $taskList
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function updateAction($taskList)
     {
         return $this->render('GoogleApiTaskBundle:Home:update.html.twig');
     }
 
 
+    /**
+     * Action to print the taskList items from google API Tasks
+     *
+     * @param $taskList
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function showItemsAction($taskList)
     {
         $taskService = $this->get('google_task_api.google.home');
--- src/GoogleApiTaskBundle/Google/TaskService.php
+++ src/GoogleApiTaskBundle/Google/TaskService.php
@@ -5,10 +5,21 @@ namespace GoogleApiTaskBundle\Google;
 use HappyR\Google\ApiBundle\Services\GoogleClient;
 use Symfony\Component\Security\Core\SecurityContext;
 
+/**
+ * Class TaskService to dialog with the Google API Client from happyr
+ *
+ * @package GoogleApiTaskBundle\Google
+ */
 class TaskService {
     
     private $service;
-    
+
+    /**
+     * Constructor to initialize the Oauth connection with the token
+     *
+     * @param $client
+     * @param $security
+     */
     public function __construct($client, $security)
     {
         $token = json_decode($security->getToken()->getUser(), true);
@@ -16,19 +27,36 @@ class TaskService {
         $this->service = new \Google_Service_Tasks($googleClient);
         $googleClient->setAccessToken($token);
     }
-    
+
+    /**
+     * Return taskList from Google
+     *
+     * @return array TaskLists
+     */
     public function getTaskLists()
     {
         return $this->service->tasklists->listTasklists()->getItems();
     }
-    
-   public function getTasksFromList($taskList)
-   {
+
+    /**
+     * Return the tasks from a taskList
+     *
+     * @param $taskList
+     *
+     * @return \Google_Service_Tasks_Tasks
+     */
+    public function getTasksFromList($taskList)
+    {
        return $this->service->tasks->listTasks($taskList);
-   }
-   
-   public function deleteTasksList($taskList)
-   {
+    }
+
+    /**
+     * Delete a taskList
+     *
+     * @param $taskList
+     */
+    public function deleteTasksList($taskList)
+    {
       $this->service->tasklists->delete($taskList);
-   }
+    }
 }
